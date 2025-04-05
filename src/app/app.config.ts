deeb728c625088a709c41340f3d47539885083b3
Sophia <sophia.atighehchi@yahoo.com>
2025-04-05 22:34:40 +0330

    style: add material - rtl configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { Directionality } from '@angular/cdk/bidi';
+import { Subject } from 'rxjs';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: Directionality,
+      useValue: {
+        value: 'rtl',
+        change: new Subject()
+      }
+    }
+  ]
 };
